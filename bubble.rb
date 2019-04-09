971a6571855723e2e8ac5e377d77a782e1379cdc
TERVER AOSU <terveraosu@gmail.com>
2019-04-09 11:23:06 +0100

    Linter error correction

--- bubble.rb
+++ bubble.rb
@@ -1,39 +1,39 @@
 # Bubble Sort Algorithm
 
 def bubble_sort(arr)
-    l = arr.length
-    swapped = true
-    while swapped do
-      swapped = false
-      (l - 1).times do |i|
-        if arr[i] > arr[i + 1]
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-          swapped = true
-        end
+  l = arr.length
+  swapped = true
+  while swapped do
+    swapped = false
+    (l - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
       end
     end
-    arr
   end
+  arr
+end
+
+#bubble_sort([4,3,78,2,0,2])
+#bubble_sort(["hi","hello","hey"])
   
-#   bubble_sort([4,3,78,2,0,2])
-#   bubble_sort(["hi","hello","hey"])
-  
-  def bubble_sort_by(arr)
-    l = arr.length
-    swapped = true
-    while swapped do
-      swapped = false
-      (l - 1).times do |i|
-        if yield([arr[i],arr[i+1]]) > 0
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-          swapped = true
-        end
+def bubble_sort_by(arr)
+  l = arr.length
+  swapped = true
+  while swapped do
+    swapped = false
+    (l - 1).times do |i|
+      if yield([arr[i], arr[i + 1]]) > 0
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
       end
     end
-    arr
   end
+  arr
+end
   
-#   bubble_sort_by(["hi","hello","hey"]) do |left,right|
-#     left.length - right.length
-#   end
+#bubble_sort_by(["hi","hello","hey"]) do |left,right|
+#left.length - right.length
+#end
   
